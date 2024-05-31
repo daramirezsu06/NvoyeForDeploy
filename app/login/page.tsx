1437e1eb71f154d00aa23e1061ede98dd0e3f218
sanjaikumarsankar <sanjai.sankar@essent.nl>
2024-06-01 01:19:22 +0200

    code cleanup

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+export default function Login() {
+  return (
+    <main>
+      <section>
+        <h1>Log In Page</h1>
+        <p>Welcome to the login page.</p>
+      </section>
+    </main>
+  );
+}
---
+++ app/signup/page.tsx
@@ -0,0 +1,10 @@
+export default function SignUp() {
+  return (
+    <main>
+      <section>
+        <h1>Sign Up Page</h1>
+        <p>Welcome to the Sign Up page.</p>
+      </section>
+    </main>
+  );
+}
