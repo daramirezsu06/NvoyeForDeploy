fb14d8b47fa43508371216c38771ad997339feca
sanjaikumarsankar <sanjai.sankar@essent.nl>
2024-06-01 10:14:18 +0200

    added themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+import { ThemeProvider } from '@mui/material/styles';
+
+import theme from './theme'
 import "./globals.css";
 
 export default function RootLayout({
@@ -10,7 +13,9 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <AppRouterCacheProvider>
-          {children}
+           <ThemeProvider theme={theme}>
+            {children}
+          </ThemeProvider>
        </AppRouterCacheProvider>
       </body>
     </html>
