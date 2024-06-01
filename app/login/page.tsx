b1b129d2d164e941a97c24ab5b259441d05255af
sanjaikumarsankar <sanjai.sankar@essent.nl>
2024-06-02 00:38:37 +0200

    test husky to format code

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,9 +3,13 @@ export default function Login() {
   return (
     <main>
       <section>
-        <Typography variant="h5">Log In Page</Typography>
+
+
+        <Typography variant="h5">Log In Page
+        </Typography>
         <p>Welcome to the login page.</p>
       </section>
+      
     </main>
   );
 }
