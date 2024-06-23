66436fd1d42763b228b6c7fbaf0b0f0da9381923
sanjaikumarsankar <sanjai.sankar@essent.nl>
2024-06-23 22:07:29 +0200

    redux setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from '@mui/material/styles';
 
 import theme from './theme';
 import './globals.css';
+import StoreProvider from './StoreProvider';
 
 export default function RootLayout({
   children,
@@ -10,12 +11,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        <AppRouterCacheProvider>
-          <ThemeProvider theme={theme}>{children}</ThemeProvider>
-        </AppRouterCacheProvider>
-      </body>
-    </html>
+    <StoreProvider>
+      <html lang="en">
+        <body>
+          <AppRouterCacheProvider>
+            <ThemeProvider theme={theme}>{children}</ThemeProvider>
+          </AppRouterCacheProvider>
+        </body>
+      </html>
+    </StoreProvider>
   );
 }
