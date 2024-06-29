86c6c7942a19fefbe4a69b8bafb6f124dfd0bc3a
sanjaikumarsankar <sanjai.sankar@essent.nl>
2024-06-30 00:07:37 +0200

    added reset password form

--- src/app/(auth)/forgot-password/components/ResetPasswordForm.tsx
+++ src/app/(auth)/forgot-password/components/ResetPasswordForm.tsx
@@ -1,3 +1,55 @@
+import { Button, Link, Stack, TextField, Typography } from '@mui/material';
+
 export default function ResetPasswordForm() {
-  return <></>;
+  return (
+    <Stack>
+      <Typography variant="h4" align="center" sx={{ marginBottom: 1 }}>
+        Reset Password
+      </Typography>
+      <form>
+        <TextField
+          label="Enter new password"
+          type="password"
+          fullWidth
+          variant="outlined"
+          InputProps={{
+            sx: { borderRadius: '16px' },
+          }}
+          required
+          margin="normal"
+        />
+        <TextField
+          label="Confirm password"
+          type="password"
+          fullWidth
+          variant="outlined"
+          InputProps={{
+            sx: { borderRadius: '16px', marginBottom: 1 },
+          }}
+          required
+          margin="normal"
+        />
+        <Button
+          sx={{ borderRadius: '8px', marginBottom: 1.5 }}
+          type="submit"
+          variant="contained"
+          color="primary"
+          size="large"
+          fullWidth
+        >
+          Update password
+        </Button>
+        <Stack sx={{ marginBottom: 1.5 }}>
+          <Typography variant="body1" align="center" gutterBottom>
+            Having trouble?
+          </Typography>
+          <Typography align="center">
+            <Link href="#" variant="body2" align="center">
+              Visit our help center
+            </Link>
+          </Typography>
+        </Stack>
+      </form>
+    </Stack>
+  );
 }
