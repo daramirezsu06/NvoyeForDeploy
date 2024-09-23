052175a31163ad3a18bf5d737b5aa289d46886fd
daramirezsu06 <daramirezsu06@gmail.com>
2024-09-23 13:32:08 -0500

    languages levels and diplomaticDtep3

--- src/app/(profile)/profile/components/ProfileCompletion.tsx
+++ src/app/(profile)/profile/components/ProfileCompletion.tsx
@@ -1,40 +1,46 @@
+import { useRedirectionProfile } from '@/src/app/hooks/useRedirectConditional';
 import { Box, Button, Stack, Typography } from '@mui/material';
 
-export const ProfileCompletion: React.FC = () => (
-  <Stack
-    spacing={2}
-    alignItems="center"
-    justifyContent={'center'}
-    sx={{ width: '100%', height: 350, gap: 2 }}
-  >
-    <Box
-      sx={{
-        width: {
-          xs: 100,
-          sm: 100,
-        },
-        height: {
-          xs: 100,
-          sm: 100,
-        },
-        borderRadius: '100%',
-        bgcolor: '#3f51b5',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: '#fff',
-        fontSize: '48px',
-      }}
+export const ProfileCompletion: React.FC = () => {
+  // Usamos el hook para obtener la función que hará la redirección condicional
+  const redirectToProfile = useRedirectionProfile();
+
+  return (
+    <Stack
+      spacing={2}
+      alignItems="center"
+      justifyContent={'center'}
+      sx={{ width: '100%', height: 350, gap: 2 }}
     >
-      ✓
-    </Box>
-    <Typography variant="h4">You’re in!</Typography>
-    <Typography variant="body1" align="center">
-      Welcome to Nvoy’s exclusive diplomatic community. Ready to take a look
-      around?
-    </Typography>
-    <Button variant="contained" color="primary">
-      Access Account
-    </Button>
-  </Stack>
-);
+      <Box
+        sx={{
+          width: {
+            xs: 100,
+            sm: 100,
+          },
+          height: {
+            xs: 100,
+            sm: 100,
+          },
+          borderRadius: '100%',
+          bgcolor: '#3f51b5',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: '#fff',
+          fontSize: '48px',
+        }}
+      >
+        ✓
+      </Box>
+      <Typography variant="h4">You’re in!</Typography>
+      <Typography variant="body1" align="center">
+        Welcome to Nvoy’s exclusive diplomatic community. Ready to take a look
+        around?
+      </Typography>
+      <Button variant="contained" color="primary" onClick={redirectToProfile}>
+        Access Account
+      </Button>
+    </Stack>
+  );
+};
